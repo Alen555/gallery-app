af9c91cbb0ed3c14bba9a8407cbc1afdfdab2f03
Alen Mujkić <amujkic1990@hotmail.com>
2017-10-28 13:55:12 +0200

    added images

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { GalleryComponent } from './gallery/gallery.component';
+import { ImageComponent } from './image/image.component';
+import { ImageDetailComponent } from './image/image-detail/image-detail.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    GalleryComponent,
+    ImageComponent,
+    ImageDetailComponent
   ],
   imports: [
     BrowserModule
